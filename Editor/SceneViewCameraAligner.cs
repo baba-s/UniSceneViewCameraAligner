16df9d8168a0f2dea197c7f39c339a52a029c18c
baba_s <shotababa1206@gmail.com>
2019-09-30 16:38:15 +0900

    first commit

---
+++ Editor/SceneViewCameraAligner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	[InitializeOnLoad]
+	internal static class SceneViewCameraAligner
+	{
+		private const string ITEM_NAME = "Edit/Scene View Camera Align";
+
+		static SceneViewCameraAligner()
+		{
+			EditorApplication.update += OnUpdate;
+		}
+
+		[MenuItem( ITEM_NAME )]
+		private static void Toggle()
+		{
+			var isOn = Menu.GetChecked( ITEM_NAME );
+			Menu.SetChecked( ITEM_NAME, !isOn );
+		}
+
+		private static void OnUpdate()
+		{
+			var isOn = Menu.GetChecked( ITEM_NAME );
+
+			if ( !isOn ) return;
+
+			var sceneView = SceneView.lastActiveSceneView;
+
+			if ( sceneView == null ) return;
+
+			var sceneCamera = Camera.main;
+
+			if ( sceneCamera == null ) return;
+
+			var sceneViewCamera = sceneView.camera;
+
+			if ( sceneViewCamera == null ) return;
+
+			sceneCamera.transform.position = sceneViewCamera.transform.position;
+			sceneCamera.transform.rotation = sceneViewCamera.transform.rotation;
+		}
+	}
+}
